3b44c834456a5ce9d2ccd076bd33082332076f17
arbuthnot-eth <brandon.arbuthnot@protonmail.com>
2025-07-20 22:03:12 -0400

    Reduce GET Http requests for frontend assets

--- frontend-privium/vite.config.ts
+++ frontend-privium/vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 import { cloudflare } from "@cloudflare/vite-plugin";
+import { singleFileCompression } from "vite-plugin-singlefile-compression";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [react(), singleFileCompression(), cloudflare()],
 })
