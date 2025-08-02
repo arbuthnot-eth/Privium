d16a4391545c27df5423859653bb24de42b4327c
arbuthnot-eth <brandon.arbuthnot@protonmail.com>
2025-08-02 07:30:23 -0400

    reduce iterations to 100000 to be compatible with cloudflare workers

--- src/authMiddleware.ts
+++ src/authMiddleware.ts
@@ -740,7 +740,7 @@ async function deriveKeyFromToken(tokenStr: string, env: Env): Promise<CryptoKey
 		{
 			name: 'PBKDF2',
 			salt: salt,
-			iterations: 600000,
+			iterations: 100000,
 			hash: 'SHA-256'
 		},
 		keyMaterial,
